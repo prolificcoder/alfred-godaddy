9865a660248b16e6dd6333707f98e1b95d71ddcd
Satyajit Malugu <smalugu@godaddy.com>
2015-02-21 14:52:41 -0800

    before clearning up

--- workflow/main.rb
+++ workflow/main.rb
@@ -2,40 +2,32 @@
 # encoding: utf-8
 
 require "alfred"
+require "net/http"
+require "uri"
+require "json"
 
-
+KEY = "UzXLycyn_7AJQ3KqhTdJ7fcFoCvoYFn"
+SECRET = "7AJTvDNyecPFeJWhn339Ze"
+SHOPPER_ID = "79427493"
 Alfred.with_friendly_error do |alfred|
   fb = alfred.feedback
   query = ARGV[0]
 
   # add a file feedback
-  fb.add_file_item(File.expand_path "~/Applications/")
 
-  # add an arbitrary feedback
-  fb.add_item({
-    :uid      => ""                     ,
-    :title    => query,
-    :subtitle => query,
-    :arg      => query,
-    :valid    => "yes"                  ,
-  })
-  
-  # add an feedback to test rescue feedback
-  fb.add_item({
-    :uid          => ""                     ,
-    :title        => "Rescue Feedback Test" ,
-    :subtitle     => "rescue feedback item" ,
-    :arg          => ""                     ,
-    :autocomplete => "failed"               ,
-    :valid        => "no"                   ,
-  })
+  uri = URI.parse("https://api.ote-godaddy.com:443/v1/domains/available?domain=#{query}&forTransfer=false&checkType=fast&waitMs=1000")
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  request = Net::HTTP::Get.new(uri.request_uri)
+  # request.initialize_http_header({
+  #     "Authorization" => "sso-key #{KEY}:#{SECRET}",
+  #     "X-Shopper-Id" => "#{SHOPPER_ID}"
+  # })
 
-  if ARGV[0].eql? "failed"
-    alfred.with_rescue_feedback = true
-    raise Alfred::NoBundleIDError, "Wrong Bundle ID Test!"
-  end
+  response = http.request(request)
 
-  puts fb.to_xml(ARGV)
+  puts fb.to_xml
 end
 
 
