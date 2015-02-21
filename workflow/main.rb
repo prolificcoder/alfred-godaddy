f00a1547e7ab77754840a9668a03fb432406e27d
Satyajit Malugu <smalugu@godaddy.com>
2015-02-21 15:11:16 -0800

    now i see errors

--- workflow/main.rb
+++ workflow/main.rb
@@ -11,23 +11,31 @@ SECRET = "7AJTvDNyecPFeJWhn339Ze"
 SHOPPER_ID = "79427493"
 Alfred.with_friendly_error do |alfred|
   fb = alfred.feedback
+  alfred.with_rescue_feedback = true
   query = ARGV[0]
 
-  # add a file feedback
-
+  # # add a file feedback
+  fb.add_file_item(File.expand_path "~/Applications/")
+  #
   uri = URI.parse("https://api.ote-godaddy.com:443/v1/domains/available?domain=#{query}&forTransfer=false&checkType=fast&waitMs=1000")
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = true
   http.verify_mode = OpenSSL::SSL::VERIFY_NONE
   request = Net::HTTP::Get.new(uri.request_uri)
-  # request.initialize_http_header({
-  #     "Authorization" => "sso-key #{KEY}:#{SECRET}",
-  #     "X-Shopper-Id" => "#{SHOPPER_ID}"
-  # })
+  request.content_type = "application/json"
 
   response = http.request(request)
 
-  puts fb.to_xml
+  # add an arbitrary feedback
+  fb.add_item({
+                  :uid => "" ,
+                  :subtitle => "#{query}",
+                  :title => "#{response['available']}" ,
+                  :arg => "A test feedback Item" ,
+                  :valid => "yes" ,
+              })
+
+  puts fb.to_xml(ARGV)
 end
 
 
