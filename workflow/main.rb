f25d596169cbbd681cd2e62c6ae16f687af1d764
Satyajit Malugu <smalugu@godaddy.com>
2015-02-21 21:34:39 -0800

    Refactored into a helper so that suggest can use the same helper.

--- workflow/main.rb
+++ workflow/main.rb
@@ -10,6 +10,17 @@ require 'openssl'
 KEY = "UzXLycyn_7AJQ3KqhTdJ7fcFoCvoYFn"
 SECRET = "7AJTvDNyecPFeJWhn339Ze"
 SHOPPER_ID = "79427493"
+
+def make_api_query(query,search_type,query_parameter)
+  uri = URI.parse("https://api.ote-godaddy.com:443/v1/domains/#{search_type}?#{query_parameter}=#{query}&checkType=fast&waitMs=1000")
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  request = Net::HTTP::Get.new(uri.request_uri)
+  request.content_type = "application/json"
+  response = http.request(request)
+end
+
 Alfred.with_friendly_error do |alfred|
   fb = alfred.feedback
   alfred.with_rescue_feedback = true
@@ -17,15 +28,7 @@ Alfred.with_friendly_error do |alfred|
 
   # # add a file feedback
   fb.add_file_item(File.expand_path "~/Applications/")
-  #
-  uri = URI.parse("https://api.ote-godaddy.com:443/v1/domains/available?domain=#{query}&forTransfer=false&checkType=fast&waitMs=1000")
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-  request = Net::HTTP::Get.new(uri.request_uri)
-  request.content_type = "application/json"
-
-  response = http.request(request)
+  response = make_api_query(query,'available','domain')
 
   alfred.ui.debug response.code
 
@@ -49,11 +52,10 @@ Alfred.with_friendly_error do |alfred|
   else
     fb.add_item({
                     :subtitle => "exactmatch needs complete domain",
-                    :title => "Enter complete domain",
+                    :title => "API didn't respond with valid data",
                     :valid => "no",
                 })
   end
-
   puts fb.to_xml
 end
 
